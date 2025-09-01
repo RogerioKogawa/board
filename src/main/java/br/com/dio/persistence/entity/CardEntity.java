4e75784924f211c9d73fd6fe3e690e1f9247a231
Rogério Kogawa de Moraes <68359041+RogerioKogawa@users.noreply.github.com>
2025-09-01 00:06:42 -0300

    Update CardEntity.java

--- src/main/java/br/com/dio/persistence/entity/CardEntity.java
+++ src/main/java/br/com/dio/persistence/entity/CardEntity.java
@@ -1,6 +1,7 @@
 package br.com.dio.persistence.entity;
 
 import lombok.Data;
+import java.time.OffsetDateTime;
 
 @Data
 public class CardEntity {
@@ -9,5 +10,7 @@ public class CardEntity {
     private String title;
     private String description;
     private BoardColumnEntity boardColumn = new BoardColumnEntity();
+    private OffsetDateTime createdAt;
+    private OffsetDateTime movedAt;
 
 }
